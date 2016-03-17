90bfcd737d1a41557838947f2781500086439234
Chris Bee <chris@ctrlbee.com>
2016-03-16 23:19:11 -0700

    practice session

---
+++ blackjack/blackjack.js
@@ -0,0 +1,29 @@
+//Create a blackjack game
+
+/*
+Rules
+N players play against dealer
+Total of cards must be below 21 
+Goal is to beat dealer score without going over 21
+Players can hit or stand 
+Aces are special - 1 or 11
+
+
+Players
+- Dealer true
+- score 0
+- cards []
+- actions: hit(), stand()
+-- if bust, score = -1
+
+Deck
+- cards []
+- shuffle
+
+Game 
+- Players []
+- Start game, deal cards
+- End game, determine winner
+
+
+*/
